992f632d6a3d77642f3353cbeeb6ec7a87cd5184
Tomas1802 <Tomas1802@users.noreply.github.com>
2021-10-02 17:30:53 +0000

    deployed via Gatsby Publish Action 🎩 for a87cd63d89ff1253c3d978eafbd5e8b590a4dd9b

---
+++ 17007de1-07409d8be276a49e99ba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[876],{5538:function(t,r,l){l.d(r,{wz_:function(){return e}});var a=l(1046);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{d:"M1.5 1H6v1H2v12h12v-4h1v4.5l-.5.5h-13l-.5-.5v-13l.5-.5z"}},{tag:"path",attr:{d:"M15 1.5V8h-1V2.707L7.243 9.465l-.707-.708L13.293 2H8V1h6.5l.5.5z"}}]})(t)}}}]);
+//# sourceMappingURL=17007de1-07409d8be276a49e99ba.js.map
---
+++ 31664189-b63552a63c139e3fc645.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[609],{9295:function(t,l,r){r.d(l,{lO3:function(){return a}});var e=r(1046);function a(t){return(0,e.w_)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M11 1l-5 5h-3l-3 4c0 0 3.178-0.885 5.032-0.47l-5.032 6.47 6.592-5.127c0.919 2.104-0.592 5.127-0.592 5.127l4-3v-3l5-5 1-5-5 1z"}}]})(t)}}}]);
+//# sourceMappingURL=31664189-b63552a63c139e3fc645.js.map
